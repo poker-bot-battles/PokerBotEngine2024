2e07d3dc54cf5e3f36e25d48b843197bb9a3cabe
Christian Alexander Hetling <chrhe@itu.dk>
2024-02-14 14:56:04 +0100

    remove unused imports

--- javabot/Observable.java
+++ javabot/Observable.java
@@ -1,11 +1,8 @@
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
