929ec37c79fb7539c96f711f79c7e8a17e8a2bbe
Frederik Petersen <fred.p@live.dk>
2024-02-16 23:01:41 +0100

    Fix not working java wrapper

--- javabot/java_wrapper.py
+++ javabot/java_wrapper.py
@@ -3,7 +3,10 @@ import subprocess
 import json
 class Bot:
   def __init__(self) -> None:
-     subprocess.run(["javac","-cp","./javabot:javabot/libs/*", "javabot/bot.java"])
+    subprocess.run(["javac","-cp","./javabot:javabot/libs/*", "javabot/PokerUtilities.java"])
+    subprocess.run(["javac","-cp","./javabot:javabot/libs/*", "javabot/Observable.java"])
+    subprocess.run(["javac","-cp","./javabot:javabot/libs/*", "javabot/bot.java"])
+
   def get_name(self):
       return "Java Bot"
 
@@ -22,6 +25,7 @@ class Bot:
                'myHandType': str(obs.get_my_hand_type()),
                'boardHandType': str(obs.get_board_hand_type()),
     }
+    # print(obsdict)
     res = subprocess.run(["java", "-cp","./javabot:javabot/libs/*", "bot", json.dumps(obsdict)], capture_output=True, text=True)
     # print("sterr     ", res.stderr)
     # print("stdout   ", res.stdout)
