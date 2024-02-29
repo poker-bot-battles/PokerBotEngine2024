0e0c89a7804e18dc8cc1d21240066a525fe23bbb
Frederik Petersen <fred.p@live.dk>
2024-02-29 17:13:50 +0100

    Add running options for vscode

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import sys
 from test import run_benchmark, run_table
 from poker_game_runner.bots import randombot
 from example_bots import panic_bot, odds_bot, checkmate, position_bot
 import my_bot
 import javabot.java_wrapper as java_wrapper
-bots = [java_wrapper, odds_bot, panic_bot]
+bots = [odds_bot, panic_bot]
 
-run_table(bots)
+lang, type = sys.argv[1], sys.argv[2]
 
-# run_benchmark(bots, 100)
+if lang == "java":
+    bots.append(java_wrapper)
+else:
+    bots.append(my_bot)
+
+if type == "benchmark":
+    run_benchmark(bots, int(sys.argv[3]))
+else:
+    run_table(bots)
+
 
